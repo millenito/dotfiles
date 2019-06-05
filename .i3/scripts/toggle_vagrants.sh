f294436ec9c2eff50cfb0b946c5a8f4d18d6ad77
millenito <wownito31@gmail.com>
2019-06-06 03:00:35 +0700

    change env variable to CAPS & add localan php 7 (commented)

--- .i3/scripts/toggle_vagrants.sh
+++ .i3/scripts/toggle_vagrants.sh
@@ -5,7 +5,9 @@ PROCESS=VBoxHeadless
 ps_out=`ps -ef | grep $PROCESS | grep -v 'grep' | grep -v $0`
 result=$(echo $ps_out | grep "$PROCESS")
 if [[ "$result" != "" ]];then
-    cd $p5 && vagrant halt
+    cd $P5 && vagrant halt
+    # cd $P7 && vagrant halt
 else
-    cd $p5 && vagrant up
+    cd $P5 && vagrant up
+    # cd $P7 && vagrant up
 fi
