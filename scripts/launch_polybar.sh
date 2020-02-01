a645256261b3c71cf4db826d3511b42623be3370
millenito <wownito31@gmail.com>
2020-02-01 20:51:56 +0700

    launch top and bottom bar for polybar powerline theme

--- scripts/launch_polybar.sh
+++ scripts/launch_polybar.sh
@@ -7,7 +7,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar bar -r &
+# polybar bar -r &
+ polybar top -r &
+ polybar bottom -r &
 
 # Hide polybar tray
 hideIt.sh --name '^Polybar tray window$' --region 0x1080+10+-40
