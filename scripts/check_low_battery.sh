2b9b5d99e54593cf90a1f6247c7af0e4024378a4
millenito <wownito31@gmail.com>
2020-02-28 02:20:35 +0700

    add battery low check for crontab

---
+++ scripts/check_low_battery.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Send notification with notify-send if battery is below 15%
+# for use in crontab
+
+STATUS=$(acpi --battery | grep Discharging)
+IMAGE="/usr/share/icons/Papirus-Dark/16x16/panel/xfce4-battery-critical.svg"
+
+if [ -n "$STATUS" ]; then
+    echo "$STATUS" | awk -F, '/Discharging/ { if (int($2) < 15) print }' | xargs -ri env DISPLAY=:0 notify-send -a "Battery status" -u critical -i "$IMAGE" -t 3000 "{}\nBattery Low."
+fi
