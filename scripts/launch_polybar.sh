d609c05e6659d86fbd1fce86822fb989bd4ca074
millenito <wownito31@gmail.com>
2020-05-12 00:37:12 +0700

    show polybar on all monitors

--- scripts/launch_polybar.sh
+++ scripts/launch_polybar.sh
@@ -8,8 +8,15 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 # polybar bar -r &
- polybar top -r &
- polybar bottom -r &
+if type xrandr; then
+    for i in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$i polybar top -r &
+        MONITOR=$i polybar bottom -r &
+    done
+else
+    polybar top -r &
+    polybar bottom -r &
+fi
 
 # Hide polybar tray
 hideIt.sh --name '^Polybar tray window$' --region 0x1080+10+-40
