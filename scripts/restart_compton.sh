ba7ce122a3372e4b20834d8850083f74aafdea83
millenito <wownito31@gmail.com>
2020-06-18 01:14:56 +0700

    remove unused scripts

--- scripts/restart_compton.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
- 
-killall compton; compton -b
-notify-send 'tranparency on'
-
--- scripts/vim_keys_global.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Load xmodmap untuk membuat keys vim h,j,k,l menggantikan arrow keys secara global sampai ke system menggunakan ALT + h/j/k/l
-
-user=nito
-
-xmodmap /home/$user/.i3/scripts/.xmodmap
