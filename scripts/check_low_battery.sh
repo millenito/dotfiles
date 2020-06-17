2c19b3227a9538bf218c8e16e07afe29db14329a
millenito <wownito31@gmail.com>
2020-06-18 01:44:01 +0700

    only use the first battery (battery 0)

--- scripts/check_low_battery.sh
+++ scripts/check_low_battery.sh
@@ -3,9 +3,9 @@
 # Send notification with notify-send if battery is below 15%
 # for use in crontab
 
-STATUS=$(acpi --battery | grep Discharging)
+STATUS=$(acpi --battery | grep "Battery 0" | grep Discharging)
 IMAGE="/usr/share/icons/Papirus-Dark/16x16/panel/xfce4-battery-critical.svg"
 
-if [ -n "$(acpi --battery | grep Discharging)" ]; then
-    echo $(acpi --battery | grep Discharging) | awk -F, '/Discharging/ { if (int($2) < 15) print }' | xargs -ri env DISPLAY=:0 notify-send -a "Battery status" -u critical -i "$IMAGE" -t 3000 "{}\nBattery Low."
+if [ -n "$STATUS" ]; then
+    echo "$STATUS" | awk -F, '/Discharging/ { if (int($2) < 15) print }' | xargs -ri env DISPLAY=:0 notify-send -a "Battery status" -u critical -i "$IMAGE" -t 3000 "{}\nBattery Low."
 fi
