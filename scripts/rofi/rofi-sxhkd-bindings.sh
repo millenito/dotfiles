1673c8a0069895e0b73ce8c81508690a1c118510
millenito <wownito31@gmail.com>
2020-06-18 00:59:52 +0700

    add some rofi scripts

--- scripts/rofi/rofi-sxhkd-bindings.sh
+++ scripts/rofi/rofi-sxhkd-bindings.sh
@@ -4,7 +4,7 @@
 # remember to always put comments above the bindings in sxhkdrc
 
 if [ -f ~/.config/sxhkd/sxhkdrc ]; then
-    PICK=$(cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu -i -no-show-icons -p "Sxhkd")
+    PICK=$(cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu -i -no-show-icons -p "Sxhkd" -width 1250)
     KEYS=$(echo "$PICK" | awk 'BEGIN { FS="#" } {print $1}')
     COMMENTS=$(echo "$PICK" | awk 'BEGIN { FS="# " } {print $2}')
     notify-send "$KEYS" "$COMMENTS"
---
+++ scripts/rofi/rofi-umount.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# A dmenu prompt to unmount drives.
+# Provides you with mounted partitions, select one to unmount.
+# Drives mounted at /, /boot and /home will not be options to unmount.
+
+unmountusb() {
+    [ -z "$drives" ] && exit
+    chosen="$(echo "$drives" | rofi -dmenu -i -p "Unmount which drive?")" || exit 1
+    chosen="$(echo "$chosen" | awk '{print $1}')"
+    [ -z "$chosen" ] && exit
+    sudo -A umount "$chosen" && notify-send "💻 USB unmounting" "$chosen unmounted."
+}
+
+unmountandroid() { \
+    chosen="$(awk '/simple-mtpfs/ {print $2}' /etc/mtab | rofi -dmenu -i -p "Unmount which device?")" || exit 1
+    [ -z "$chosen" ] && exit
+    sudo -A umount -l "$chosen" && notify-send "🤖 Android unmounting" "$chosen unmounted."
+}
+
+asktype() { \
+    choice="$(printf "USB\\nAndroid" | rofi -dmenu -i -p "Unmount a USB drive or Android device?")" || exit 1
+    case "$choice" in
+        USB) unmountusb ;;
+        Android) unmountandroid ;;
+    esac
+}
+
+drives=$(lsblk -nrpo "name,type,size,mountpoint" | awk '$4!~/\/boot|\/home$|SWAP/&&length($4)>1{printf "%s (%s)\n",$4,$3}')
+
+if ! grep simple-mtpfs /etc/mtab; then
+    [ -z "$drives" ] && echo "No drives to unmount." &&  exit
+    msg="Unmountable USB drive detected."
+    echo "$msg"; notify-send "$msg"
+    unmountusb
+else
+    if [ -z "$drives" ]
+    then
+        msg="Unmountable Android device detected."
+        echo "$msg"; notify-send "$msg"
+        unmountandroid
+    else
+        msg="Unmountable USB drive(s) and Android device(s) detected."
+        echo "$msg"; notify-send "$msg"
+        asktype
+    fi
+fi
