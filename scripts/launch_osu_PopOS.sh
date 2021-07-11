10a962025d5b8dc4ace96a3e31ec7c46318ae39a
millenito <wownito31@gmail.com>
2021-07-12 01:22:36 +0700

    launch osu script for multi monitor pop os

---
+++ scripts/launch_osu_PopOS.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# disable multi monitors before launching osu
+if [ $(xrandr | grep -v "disconnected" | grep "HDMI" | wc -l) -eq 1 ]; then
+    xrandr --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --off
+fi
+
+# Run osu! then go back to using multiple monitors
+lutris lutris:rungameid/1 && xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
