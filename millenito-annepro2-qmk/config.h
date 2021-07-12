07463a97efa19698066e1c0f1862250512b6947a
millenito <wownito31@gmail.com>
2021-07-12 21:17:16 +0700

    add retro tapping and shorter tap delay

--- millenito-annepro2-qmk/config.h
+++ millenito-annepro2-qmk/config.h
@@ -1,2 +1,8 @@
 // Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
 #define PERMISSIVE_HOLD
+
+// Define tap or hold delay in ms
+#define TAPPING_TERM 150
+
+// Send mod if released without pressing another key
+#define RETRO_TAPPING
