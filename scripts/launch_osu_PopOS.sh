d66f02ed478339b2dd670870ca5444e9998b88dc
millenito <wownito31@gmail.com>
2021-09-19 19:27:32 +0700

    launch opentabletdriver before osu

--- scripts/launch_osu_PopOS.sh
+++ scripts/launch_osu_PopOS.sh
@@ -5,5 +5,8 @@ if [ $(xrandr | grep -v "disconnected" | grep "HDMI" | wc -l) -eq 1 ]; then
     xrandr --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --off
 fi
 
-# Run osu! then go back to using multiple monitors
-lutris lutris:rungameid/1 && xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+# Run OpenTabletDriver
+/usr/bin/opentabletdriver &
+
+# Run osu! then go back to using multiple monitors and kill OpenTabletDriver
+lutris lutris:rungameid/1 && xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal && pkill -f "OpenTabletDrive"
