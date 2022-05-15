191abbd19f2ad467900ea983900bdd47ba87ddc0
millenito <wownito31@gmail.com>
2022-05-15 22:34:33 +0700

    add macos version (switch win and alt position)

---
+++ millenito-annepro2-qmk-macos/config.h
@@ -0,0 +1,23 @@
+// Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
+#define PERMISSIVE_HOLD
+
+// Define tap or hold delay in ms
+#define TAPPING_TERM 150
+
+// Send mod if released without pressing another key
+#define RETRO_TAPPING
+
+// Define amount of combos to be used
+#define COMBO_COUNT 1 
+
+// Define amount of taps for layer toggles
+#define TAPPING_TOGGLE 2
+
+// Mouse key settings
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
+#define MOUSEKEY_WHEEL_INTERVAL 50 // The default is 100
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 100 // The default is 40
